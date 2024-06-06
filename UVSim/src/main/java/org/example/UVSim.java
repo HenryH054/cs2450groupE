be2a9ac47ed8ad7495bbabf4e0e41ea03694a6d1
Damon Morgan <102624824+thebrigman@users.noreply.github.com>
2024-06-05 22:22:00 -0600

    readme updated

--- UVSim/src/main/java/org/example/UVSim.java
+++ UVSim/src/main/java/org/example/UVSim.java
@@ -12,7 +12,7 @@ public class UVSim {
         UVSim uvSim = new UVSim();
         int instruction;
 
-        File file = new File("./src/main/java/Test1.txt");
+        File file = new File(args[0]);
         Scanner scannerFile = new Scanner(file);
 
         while (scannerFile.hasNextLine()) {
@@ -65,6 +65,7 @@ public class UVSim {
     }
     
     public void read(int operand) {
+        System.out.println("Enter number");
         Scanner input = new Scanner(System.in);
         memory[operand] = input.nextInt();
         input.close();
