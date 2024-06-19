45cf52a64ae4b635f79cadbddcb3478a78630cac
ernestofelix <130931604+ernestofelix@users.noreply.github.com>
2024-06-19 17:08:43 -0600

    Add files via upload

---
+++ UVSim/src/main/java/org/example/business/ArithmeticOperations.java
@@ -0,0 +1,30 @@
+package org.example.business;
+
+public class ArithmeticOperations {
+    private CPU cpu;
+
+    public ArithmeticOperations(CPU cpu) {
+        this.cpu = cpu;
+    }
+
+    public void add(int operand) {
+        cpu.accumulator += cpu.memory.getData(operand);
+    }
+
+    public void subtract(int operand) {
+        cpu.accumulator -= cpu.memory.getData(operand);
+    }
+
+    public void divide(int operand) {
+        int data = cpu.memory.getData(operand);
+        if (data != 0) {
+            cpu.accumulator /= data;
+        } else {
+            throw new ArithmeticException("Division by zero");
+        }
+    }
+
+    public void multiply(int operand) {
+        cpu.accumulator *= cpu.memory.getData(operand);
+    }
+}
---
+++ UVSim/src/main/java/org/example/business/BranchOperations.java
@@ -0,0 +1,30 @@
+package org.example.business;
+
+public class BranchOperations {
+    private CPU cpu;
+
+    public BranchOperations(CPU cpu) {
+        this.cpu = cpu;
+    }
+
+    public void branch(int operand) {
+        cpu.programCounter = operand;
+    }
+
+    public void branchNeg(int operand) {
+        if (cpu.accumulator < 0) {
+            cpu.programCounter = operand;
+        }
+    }
+
+    public void branchZero(int operand) {
+        if (cpu.accumulator == 0) {
+            cpu.programCounter = operand;
+        }
+    }
+
+    public void halt() {
+        System.out.println("Program execution halted.");
+        cpu.programCounter = -1;
+    }
+}
