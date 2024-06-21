8c90293377ccae16f86773eb2d2e6c09520a9595
Damon Morgan <102624824+thebrigman@users.noreply.github.com>
2024-06-20 23:05:13 -0600

    update

---
+++ UVSim/src/test/java/org/example/UVSimGUITest.java
@@ -0,0 +1,20 @@
+package test.java.org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UVSimGUITest {
+
+    @Test
+    void createAndShowGUI() {
+    }
+
+    @Test
+    void appendOutput() {
+    }
+
+    @Test
+    void getInputField() {
+    }
+}
