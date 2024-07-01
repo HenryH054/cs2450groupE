f20ef46fb1f41418987c6091e078cdcec2f9c432
Damon Morgan <102624824+thebrigman@users.noreply.github.com>
2024-07-01 08:59:47 -0600

    update

--- UVSim/src/main/java/org/example/business/UVSim.java
+++ UVSim/src/main/java/org/example/business/UVSim.java
@@ -2,6 +2,10 @@ package main.java.org.example.business;
 
 import main.java.org.example.controller.UVSimGUI;
 
+/**
+ * Entry point for the UVSim application.
+ * @author Damon Morgan
+ */
 public class UVSim {
 
     public static void main(String[] args) {
